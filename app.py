79fa72ca5e6b2eb9f7fcf79dd27b9683da06831e
werkinehadane-cmd <werkinehadane@gmail.com>
2026-03-13 05:59:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 import os
 
@@ -5,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
-        return render_template('index.html')
+    # ከታች ያለው መስመር የግድ 4 ባዶ ቦታ (Spaces) ገባ ማለት አለበት
+    return render_template('index.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    # ከታች ያለው መስመር የግድ 4 ባዶ ቦታ (Spaces) ገባ ማለት አለበት
     app.run(host='0.0.0.0', port=port)
