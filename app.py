ebae2ce64c5ec093e33181412c6d8ad8c9561917
werkinehadane-cmd <werkinehadane@gmail.com>
2026-03-13 05:26:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # ይህ መስመር በ templates ውስጥ ያለውን index.html እንዲከፍት ያደርገዋል
+    # index.html በ templates ፎልደር ውስጥ መሆኑን ያረጋግጣል
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render ላይ እንዲሰራ ይህ መስመር የግድ ያስፈልጋል
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
